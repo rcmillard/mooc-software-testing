181ae89c6745d127bafd957b504781aab5f0f1a0
unknown <rebecca.millard@kainexus.com>
2020-01-17 13:18:09 -0500

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -19,4 +19,10 @@ public class GettingStartedTest {
         Assertions.assertEquals(5, result);
     }
 
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+
 }
